ab1a8d9554e01388ba11c045804c6bdd11c1aadc
hk-none <68328936+chk-n@users.noreply.github.com>
2024-01-01 03:29:18 +0100

    Update utils.go

--- src/utils.go
+++ src/utils.go
@@ -1,8 +1,23 @@
 package dash
 
+import "encoding/json"
+
 func prependBytes(s []byte, v byte) []byte {
 	s = append(s, v)
 	copy(s[1:], s)
 	s[0] = v
 	return s
+}
+
+func structToStr(s any) (*string, error) {
+	b, err := json.Marshal(s)
+	if err != nil {
+		return nil, err
+	}
+	str := string(b)
+	return &str, nil
+}
+
+func strToStruct(s string, d any) error {
+	return json.Unmarshal([]byte(s), &d)
 }
